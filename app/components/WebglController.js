caaf4809715fa1857b5380262b0fcdf06e60287f
codingInSpace <jonathan.grangien@gmail.com>
2015-12-06 16:44:44 +0100

    Add example controller and wegbl directive

---
+++ app/components/WebglController.js
@@ -0,0 +1,10 @@
+angular.module('ccApp')
+  .controller('WebglController', ['$scope', function ($scope) {
+
+    $scope.canvasWidth = 400;
+    $scope.canvasHeight = 400;
+    $scope.dofillcontainer = true;
+    $scope.scale = 1;
+    $scope.materialType = 'lambert';
+
+  }]);
