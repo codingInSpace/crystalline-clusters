1074c3ead495036e19eae94fb149c95d539a02e1
codingInSpace <jonathan.grangien@gmail.com>
2015-12-09 16:15:38 +0100

    Implement floaty Dm scale loop

--- app/components/ccAudio.js
+++ app/components/ccAudio.js
@@ -5,12 +5,65 @@ angular.module('ccApp')
     return {
       restrict: 'A',
       link: function postLink(scope, element, attrs) {
+        var scale_1 = ["D3", "E3", "F3", "G3", "A4", "Bb4", "C4"];
+        // scale_2 = [];
 
-        //create one of Tone's built-in synthesizers and connect it to the master output
-        var synth = new Tone.SimpleSynth().toMaster();
+        var osc = new Tone.Oscillator(440, "square");
 
-        //play a middle c for the duration of an 8th note
-        synth.triggerAttackRelease("C4", "8n");
+        // feedback
+        var feedbackDelay = new Tone.PingPongDelay("8n", 0.6);
+        osc.connect(feedbackDelay);
+        feedbackDelay.toMaster();
+
+        // panner
+        var panner = new Tone.AutoPanner(0.5);
+        panner.toMaster();
+        // panner.setDry(0.3);
+        osc.connect(panner);
+
+        // envelope
+        // var env = new Tone.Envelope(2.5, 0.1, 0.1, 0.2);
+        // env.connect(osc.output.gain);
+
+        //connect it to the output
+        //osc.setVolume(-10);
+        osc.toMaster();
+        // osc.start();
+
+        var osc_bg = new Tone.Oscillator(440, "sine");
+        osc_bg.toMaster();
+        // osc_bg.start();
+        osc_bg.volume.value = -6;
+
+        // vibrato
+        var vibrato = new Tone.LFO(6, -25, 25);
+        vibrato.start();
+        vibrato.connect(osc_bg.detune);
+
+        // feedback
+        var feedbackDelay = new Tone.PingPongDelay("8n", 0.2);
+        osc_bg.connect(feedbackDelay);
+        feedbackDelay.toMaster();
+        // feedbackDelay.setWet(0.8);
+
+        Tone.Transport.loopEnd = "1m";
+        Tone.Transport.loop = true;
+        Tone.Transport.setInterval(function(time){
+
+        	var index = Math.floor( Math.random() * scale_1.length );
+        	var freq = osc_bg.noteToFrequency( scale_1[ index ] );
+          	// osc_bg.setFrequency(freq);
+            osc_bg.frequency.value = freq;
+        }, "4n");
+
+        // Tone.Transport.setInterval(function(time){
+        // 	var index = Math.floor( Math.random() * scale_2.length );
+        // 	var freq = osc_bg.noteToFrequency( scale_2[ index ] );
+        // 	g_objs.get("head").osc.setFrequency(freq);
+        //
+        // }, "16n");
+
+        Tone.Transport.start();
       }
     };
   });
